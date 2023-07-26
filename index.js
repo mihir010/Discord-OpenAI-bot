ad44e3693940f7ec55c3e6381ff3092f7a16237f
mihir010 <78706624+mihir010@users.noreply.github.com>
2023-07-27 03:39:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,128 @@
+// require("dotenv/config");
+// const { Client, IntentsBitField } = require("discord.js");
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: process.env.API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// const bot = new Client({
+//   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+// });
+
+// bot.on("ready", () => {
+//   console.log("the bot is online");
+// });
+
+// bot.on("messageCreate", async (message) => {
+//   // console.log(message);
+//   if (message.author.bot) return;
+//   if (message.channel.id !== process.env.CHANNEL_ID) return;
+//   if (message.content.startsWith("!")) return;
+
+//   let convoLog = [{ role: "system", content: "hello chatbot" }];
+
+//   await message.channel.sendTyping();
+
+//   let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+//   prevMessages.reverse();
+
+//   prevMessages.forEach((msg) => {
+//     if (message.content.startsWith("!")) return;
+//     if (msg.author.id !== message.author.id) return;
+//     if (msg.author.id !== bot.user.id && message.author.bot) return;
+
+//     convoLog.push({
+//       role: "user",
+//       content: message.content,
+//     });
+//   });
+
+//   const result = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: convoLog,
+//   });
+
+//   message.reply(result.data.choices[0].message);
+//   //   console.log(result.data.choices[0].message);
+// });
+
+// bot.login(process.env.TOKEN);
+
+require("dotenv/config");
+const { Client, IntentsBitField } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+// client.on('ready', () => {
+//   console.log('The bot is online!');
+// });
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith("!")) return;
+
+  let conversationLog = [
+    { role: "system", content: "You are a friendly chatbot." },
+  ];
+
+  try {
+    await message.channel.sendTyping();
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+      if (msg.content.startsWith("!")) return;
+      if (msg.author.id !== client.user.id && message.author.bot) return;
+      if (msg.author.id == client.user.id) {
+        conversationLog.push({
+          role: "assistant",
+          content: msg.content,
+          name: msg.author.username
+            .replace(/\s+/g, "_")
+            .replace(/[^\w\s]/gi, ""),
+        });
+      }
+
+      if (msg.author.id == message.author.id) {
+        conversationLog.push({
+          role: "user",
+          content: msg.content,
+          name: message.author.username
+            .replace(/\s+/g, "_")
+            .replace(/[^\w\s]/gi, ""),
+        });
+      }
+    });
+
+    const result = await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversationLog,
+        // max_tokens: 256, // limit token usage
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+    message.reply(result.data.choices[0].message);
+  } catch (error) {
+    console.log(`ERR: ${error}`);
+  }
+});
+
+client.login(process.env.TOKEN);
